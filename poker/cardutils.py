639a2f218c6eeb37e166152bf590fb0a6751b2ff
davidpendergast <ghastxyz@gmail.com>
2025-02-15 02:52:51 -0500

    logic for calculating made-hand types

--- poker/cardutils.py
+++ poker/cardutils.py
@@ -1,4 +1,5 @@
 import re
+import typing
 
 RANKS = 'AKQJT98765432'
 SUITS = ['s', 'h', 'd', 'c']
@@ -9,6 +10,7 @@ STRONG_ACES = "AK, AQ, AJ"
 BROADWAYS="KJ, KQ, QJ"
 SUITED_CONNECTORS = "32s-AKs"
 SUITED_GAPPERS = "42s-AQs"
+
 
 class HandTypes:
     # Made Hands
@@ -152,3 +154,135 @@ def cards_match_pattern(cards, pattern_code: str):
                     if cards_match_pattern(cards, f"{RANKS[i]}{RANKS[i-gap]}{suit}"):
                         return True
     return False
+
+
+def sort_by_rank(cards: typing.List[str]) -> typing.List[str]:
+    res = list(cards)
+    res.sort(key=lambda c: RANKS.index(c[0]))
+    return res
+
+
+def split_by_suits(cards) -> typing.Dict[str, typing.List[str]]:
+    res = {}
+    for c in cards:
+        if c[1] not in res:
+            res[c[1]] = []
+        res[c[1]].append(c)
+    return res
+
+
+def split_by_ranks(cards) -> typing.Dict[str, typing.List[str]]:
+    res = {}
+    for c in cards:
+        if c[0] not in res:
+            res[c[0]] = []
+        res[c[0]].append(c)
+    return res
+
+
+def fill_kickers(made_hand, cards):
+    res = list(made_hand)
+    for c in cards:
+        if len(res) >= 5:
+            return res
+        if c not in res:
+            res.append(c)
+    return res
+
+
+def calc_hand(cards):
+    """returns: (HAND_TYPE, cards)
+    """
+    cards = sort_by_rank(cards)
+    by_suit = split_by_suits(cards)
+    by_rank = split_by_ranks(cards)
+    trips = [g for g in by_rank.values() if len(g) == 3]
+    pairs = [g for g in by_rank.values() if len(g) == 2]
+
+    # STRAIGHT_FLUSHES
+    for s in by_suit:
+        if len(by_suit[s]) >= 5:
+            best = []
+            for c in by_suit[s]:
+                if len(best) == 0:
+                    best.append(c)
+                elif len(best) == 5:
+                    break
+                elif RANKS.index(best[-1][0]) == RANKS.index(c[0]) - 1:
+                    best.append(c)
+                else:
+                    best.clear()
+                    best.append(c)
+            if len(best) == 5:
+                return HandTypes.STRAIGHT_FLUSH, best
+
+    # QUADS
+    for rank_group in by_rank.values():
+        if len(rank_group) == 4:
+            best = list(rank_group)
+            return HandTypes.QUADS, fill_kickers(best, cards)
+
+    # FULL HOUSES
+    if len(trips) >= 1 and (len(trips) + len(pairs)) >= 2:
+        best = list(trips[0])
+        if len(trips) > 1:
+            best.extend(trips[1][0:2])
+        else:
+            best.extend(pairs[0])
+        return HandTypes.FULL_HOUSE, best
+
+    # FLUSHES
+    for suit_group in by_suit.values():
+        if len(suit_group) >= 5:
+            return HandTypes.FLUSH, suit_group[:5]
+
+    # STRAIGHTS
+    best_straight = []
+    for c in cards:
+        if len(best_straight) == 0:
+            best_straight.append(c)
+        elif len(best_straight) == 5:
+            break
+        elif RANKS.index(best_straight[-1][0]) == RANKS.index(c[0]) - 1:
+            best_straight.append(c)
+        elif RANKS.index(best_straight[-1][0]) == RANKS.index(c[0]):
+            pass  # dupe rank inside the straight, has no effect
+        else:
+            best_straight.clear()
+            best_straight.append(c)
+    if len(best_straight) == 5:
+        return HandTypes.STRAIGHT, best_straight
+
+    # TRIPS
+    if len(trips) == 1:
+        return HandTypes.TRIPS, fill_kickers(trips[0], cards)
+
+    # TWO_PAIRS
+    if len(pairs) >= 2:
+        return HandTypes.TWO_PAIR, fill_kickers(pairs[0] + pairs[1], cards)
+
+    # PAIRS
+    if len(pairs) == 1:
+        return HandTypes.PAIR, fill_kickers(pairs[0], cards)
+
+    # HIGH_CARDS
+    return HandTypes.HIGH_CARD, cards[0:5]
+
+
+if __name__ == "__main__":
+    tests = {
+        "str_flush":    ['Js', 'Qs', 'Qd', '3s', 'Ts', '9s', '8s'],
+        "quad_4s":      ['4h', '4d', 'Ks', '8c', '4c', '8s', '4c'],
+        "full_hs":      ['Js', 'Qs', 'Qd', '8h', 'Ts', '8c', '8s'],
+        "full_hs2":     ['Js', 'Qs', 'Qd', '8h', 'Qh', '8c', '8s'],
+        "flush":        ['Js', 'Qs', 'Qd', '3s', '2s', '9s', '8s'],
+        "straight":     ['Js', 'Qs', 'Qd', '3s', 'Tc', '9c', '8s'],
+        "trip_3s":      ['3s', '3d', '4h', 'Ks', '2d', '3h', '7d'],
+        "twp_pair":     ['As', '3d', '4h', '7s', '2d', '3h', '7d'],
+        "pair_3s":      ['As', '3d', '4h', 'Ks', '2d', '3h', '7d'],
+        "ace_high":     ['Qc', '4h', '3s', 'As', 'Ts', '9c', '8s'],
+    }
+    for t in tests:
+        print(f"{t:<16}{calc_hand(tests[t])}")
+
+    print()
